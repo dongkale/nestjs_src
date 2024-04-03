29d1fdab2a4a1630819ef6ec02aefaee79ddbe4f
LENNON\dklee <dongkale@naver.com>
2024-04-03 19:12:16 +0900

    added

---
+++ nestjs-starter-kit/api/src/db/migrations/1712111631745-Roles.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class Roles1712111631745 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
