d292ee1b04c6641819bdc08226e6d5b11aede040
LENNON\dklee <dongkale@naver.com>
2024-05-07 19:22:57 +0900

    eslint

--- nestjs-cqrs-example-06-restapi-ddd/.eslintrc.js
+++ nestjs-cqrs-example-06-restapi-ddd/.eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
@@ -14,17 +15,17 @@ module.exports = {
     node: true,
     jest: true,
   },
-  ignorePatterns: [
-    '.eslintrc.js',
-    '.aws',
-    '.github',
-    'assets',
-    '*.md',
-    '*.yaml',
-    '*.yml',
-    'Dockerfile*',
-    'node_modules',
-    '*ignore',
-    '*.json',
-  ],
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {    
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+  },
 };
