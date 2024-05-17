d538ae61425c19d66f5443f00faeaef46dcaa2d6
LENNON\dklee <dongkale@naver.com>
2024-05-17 18:50:19 +0900

    코드 정리

--- nestjs-clean-architecture-example-03-typeorm-todo-complete/src/usecases/todo/addTodo.usecases.ts
+++ nestjs-clean-architecture-example-03-typeorm-todo-complete/src/usecases/todo/addTodo.usecases.ts
@@ -1,5 +1,5 @@
 import { Logger } from '@nestjs/common';
-import { ILogger } from '@/domain/logger/logger.interface';
+// import { ILogger } from '@/domain/logger/logger.interface';
 import { TodoM } from '@/domain/model/todo';
 import { TodoRepository } from '@/domain/repositories/todoRepository.interface';
 
