2094e49e84f05dd4deb72abee810ce41c5aac64c
LENNON\dklee <dongkale@naver.com>
2024-06-20 19:16:55 +0900

    ...

--- nestjs-hexagonal-architecture-example-07-typeorm-winston-todo-complete/src/todo/inbound-port/find-todos.inbound-port.interface.ts
+++ nestjs-hexagonal-architecture-example-07-typeorm-winston-todo-complete/src/todo/inbound-port/find-todos.inbound-port.interface.ts
@@ -1,10 +1,12 @@
 // Dto의 경우 인풋 Dto, 아웃풋 Dto 를 따로 만들어서 각 레이어에 전달한다.
+import { TodoId } from '@/todo/entity/todo.entity';
+
 export type FindTodosInboundPortInputDto = void;
 
 export type FindTodosInboundPortOutputDto = Array<{
-  name: string;
-  email: string;
-  phone: string;
+  id: TodoId;
+  title: string;
+  content: string;
 }>;
 
 export interface IFindTodosInboundPort {
